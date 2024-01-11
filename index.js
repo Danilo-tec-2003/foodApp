7cb639fbaba0f9b7dc1a1b6de339a82bbc55b929
Danilo Mendes <danilodev.br@gmail.com>
2024-01-10 21:53:34 -0300

    commit final

---
+++ index.js
@@ -0,0 +1,37 @@
+//alert("óla mundo")//
+
+/*variáveis no javascript (var ou let) */
+
+//var nome= "danilo";
+//var peso= "90";
+
+//alert(`olá ${nome} seu peso é ${peso}`)
+
+//let n1 = 25
+//let n2 = 25
+
+
+
+
+//document.write(`A soma de ${n1} + ${n2} = ${n1+n2}`)
+
+//let nome = prompt ("digite seu nome")
+//let idade = prompt ("digite a sua idade")
+
+//document.write(`${nome} você é genial para a sua idade ${idade},  anos.`)
+
+
+//let numero = prompt ("digite um número")
+//numero = parseFloat (numero)
+
+//let numero2 = prompt ("digite outro número") 
+//numero2 = parseFloat  (numero2)
+
+//console.log (`A soma de ${numero} + ${numero2} = ${numero+numero2}`)
+
+
+//let n1 = parseFloat(prompt("digite um número"))
+
+//let n2 = parseFloat(prompt("digite outro número"))
+
+//document.write (`A divisão entre ${n1} e ${n2} = ${n1/n2}`)
